3f798a36be72e91ba940f518938e1fe107524b56
Pr0xximity999 <tomsomgamer999@gmail.com>
2025-06-12 14:46:55 +0200

    disabled jwt config in program.cs just so the app runs for production

--- Soepkip-API/Program.cs
+++ Soepkip-API/Program.cs
@@ -17,20 +17,20 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient<WeatherService>();
 
 // JWT Configuratie
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
-        };
-    });
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//     .AddJwtBearer(options =>
+//     {
+//         options.TokenValidationParameters = new TokenValidationParameters
+//         {
+//             ValidateIssuer = true,
+//             ValidateAudience = true,
+//             ValidateLifetime = true,
+//             ValidateIssuerSigningKey = true,
+//             ValidIssuer = builder.Configuration["Jwt:Issuer"],
+//             ValidAudience = builder.Configuration["Jwt:Audience"],
+//             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+//         };
+//     });
 //Depencency injection
 builder.Services.AddTransient<ITrashRepository, TrashRepository>();
 
